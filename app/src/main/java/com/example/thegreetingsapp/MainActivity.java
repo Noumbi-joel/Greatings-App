1f35ed0a274d81b24a0a8908a4aaf9d7a48a9fb0
Little Jacob <joelnoumbi28@gmail.com>
2022-11-02 00:22:24 +0100

    get Name from editText + print it via a Toast

--- app/src/main/java/com/example/thegreetingsapp/MainActivity.java
+++ app/src/main/java/com/example/thegreetingsapp/MainActivity.java
@@ -5,15 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    EditText editText;
+    Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText1);
+        btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Hello "+editText.getText().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
